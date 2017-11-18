0196fcca02c38dbd0e908e62c307ccf37052f59e
r-sreesaran <r.sreesaran@gmail.com>
2017-11-18 16:32:54 +0530

    added fibbonnaci problem

--- programs/src/main/java/geekforgeeks/Fibonacci.java
+++ programs/src/main/java/geekforgeeks/Fibonacci.java
@@ -1,5 +1,27 @@
 package geekforgeeks;
 
+import java.lang.reflect.Array;
+
 //http://practice.geeksforgeeks.org/problems/yeh-karo-toh-maane/0
 public class Fibonacci {
+    int[] values;
+    public void printFibbonacci() {
+        int no =1;
+        int start=0;
+        int temp=0;
+        values = new int[300];
+
+        for(int i=0;i<300;i++) {
+
+            values[i] = no;
+            temp = no;
+            no = (start+no)%100;
+            start = temp;
+        }
+    }
+
+    public void returnNo(int limit) {
+        System.out.println(values[limit%300-1]);
+    }
+
 }
--- programs/src/test/java/geeksforgeeks/FibonacciTest.java
+++ programs/src/test/java/geeksforgeeks/FibonacciTest.java
@@ -1,4 +1,14 @@
 package geeksforgeeks;
 
+import geekforgeeks.Fibonacci;
+import org.testng.annotations.Test;
+
 public class FibonacciTest {
+
+    @Test
+    public void testFibonacci() {
+        Fibonacci fib = new Fibonacci();
+        fib.printFibbonacci();
+        fib.returnNo(80493);
+    }
 }
