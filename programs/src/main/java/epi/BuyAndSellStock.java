14e9831cecaee0dcac18c51da68af54edd17b375
r-sreesaran <r.sreesaran@gmail.com>
2017-12-02 19:06:42 +0530

    find max diff of sub array

---
+++ programs/src/main/java/epi/BuyAndSellStock.java
@@ -0,0 +1,34 @@
+package epi;
+
+import java.util.Arrays;
+import java.util.List;
+
+//Find the maximum difference of the sub array
+public class BuyAndSellStock {
+
+    //imperative style programming
+    public void calculateMaxSubArrayLen(List<Integer> values) {
+
+        int minValue=values.get(0);
+        int maxDifferecnce=0;
+        for(int i=1;i<values.size();i++) {
+           if(values.get(i)<minValue)
+               minValue = values.get(i);
+            else if(maxDifferecnce<values.get(i)-minValue)
+            maxDifferecnce = values.get(i)-minValue;
+        }
+        System.out.println(maxDifferecnce);
+
+    }
+
+    //solution using functional porgramming
+
+    public void calculate(List<Integer> values) {
+//Todo
+    }
+
+    public static void main(String[] args) {
+BuyAndSellStock test= new BuyAndSellStock();
+test.calculateMaxSubArrayLen(Arrays.asList(310,315,275,295,260,270,290,230,255,250));
+    }
+}
