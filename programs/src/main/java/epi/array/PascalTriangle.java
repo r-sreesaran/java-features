b2fd201d8218d829693b3a7d4a828ac2b30c752a
r-sreesaran <r.sreesaran@gmail.com>
2017-12-17 11:32:17 +0530

    added solution pascal triangle

--- programs/src/main/java/epi/array/PascalTriangle.java
+++ programs/src/main/java/epi/array/PascalTriangle.java
@@ -1,4 +1,35 @@
 package epi.array;
 
+//https://en.wikipedia.org/wiki/Pascal%27s_triangle
+
 public class PascalTriangle {
+
+    //using two dimensional array
+    public void generatePacscalTriangle(int n) {
+
+        int values[][] = new int [n][n];
+        for(int line=0;line<n;line++) {
+            for(int j=0;j<=line;j++) {
+                if(j==0||j==line)
+                 values[line][j]=1;
+               else
+                 values[line][j] = values[line-1][j]+ values[line-1][j-1];
+
+            }
+        }
+        printPascalTriangle(values);
+    }
+
+    public void printPascalTriangle(int values[][]) {
+        for(int i=0;i<values.length;i++) {
+            for(int j=0;j<values.length;j++) {
+                System.out.println(values[i][j]);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        PascalTriangle generateTriangle = new PascalTriangle();
+        generateTriangle.generatePacscalTriangle(5);
+    }
+
 }
